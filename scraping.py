3c8e0d863739708289c0a6e5924c46d13bc1ddc1
Andrea Bianchi <bianan96@gmail.com>
2022-01-19 18:54:08 +0100

    PSNR and MSE's std deviations

--- scraping.py
+++ scraping.py
@@ -1,4 +1,6 @@
-MSE_medie = open("MSEmedie.csv", 'w')
+import numpy as np
+
+MSE_medie = open("MSEstd.csv", 'w')
 
 for i in range(8):
     MSE_values = []
@@ -8,21 +10,20 @@ for i in range(8):
         current.close()
         MSE_values.append(current_lines[i+1])
     
-    noised = 0.0
-    naive = 0.0
-    regolarized = 0.0
-    regolarized2 = 0.0
-    totvar = 0.0
+    noised = []
+    naive = []
+    regolarized = []
+    regolarized2 = []
+    totvar = []
 
     for value in MSE_values:
         fields = value.split(',')
-        #output = open(f"scraped/medie/MSE-{fields[0]}.csv", 'a')
-        noised += float(fields[1])
-        naive += float(fields[2])
-        regolarized += float(fields[3])
-        regolarized2 += float(fields[4])
-        totvar += float(fields[5])
+        noised.append(float(fields[1]))
+        naive.append(float(fields[2]))
+        regolarized.append(float(fields[3]))
+        regolarized2.append(float(fields[4]))
+        totvar.append(float(fields[5]))
 
-    MSE_medie.write(f"{MSE_values[0].split(',')[0]}, {noised/10}, {naive/10}, {regolarized/10}, {regolarized2/10}, {totvar/10}\n")
+    MSE_medie.write(f"{MSE_values[0].split(',')[0]}, {np.std(noised)}, {np.std(naive)}, {np.std(regolarized)}, {np.std(regolarized2)}, {np.std(totvar)}\n")
     
 MSE_medie.close()
